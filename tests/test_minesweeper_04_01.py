29f17e5a854d13430a648e986676775eeca077c2
KarineB <karine.bauch.web@gmail.com>
2025-04-01 12:17:11 +0200

    add test

--- tests/test_minesweeper_04_01.py
+++ tests/test_minesweeper_04_01.py
@@ -7,3 +7,17 @@
 # 1*2**2
 # 1234*2
 # 01*211
+
+def test_minesweeper():
+    data = """
+    .*.**.
+    ....*.
+    ..*...
+    """
+
+    expected = """
+    1*2**2
+    1234*2
+    01*211
+    """
+    assert minesweeper(data) == expected
