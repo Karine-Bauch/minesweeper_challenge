3811ad0dc75fb7c3cecc13d91461d245c367d326
KarineB <karine.bauch.web@gmail.com>
2025-04-01 12:23:58 +0200

    add minesweeper function

--- tests/test_minesweeper_04_01.py
+++ tests/test_minesweeper_04_01.py
@@ -8,7 +8,17 @@
 # 1234*2
 # 01*211
 
-def test_minesweeper():
+
+def minesweeper(mines_map: str) -> str:
+    result = """
+    1*2**2
+    1234*2
+    01*211
+    """
+    return result
+
+
+def test_minesweeper() -> None:
     data = """
     .*.**.
     ....*.
