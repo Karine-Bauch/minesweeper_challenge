ccba3a31547d719647d4c60ca5bd03686f8d4ade
KarineB <karine.bauch.web@gmail.com>
2025-04-02 19:05:42 +0200

    Day 2: 2e example -> GREEN

--- tests/test_minesweeper_04_02.py
+++ tests/test_minesweeper_04_02.py
@@ -1,11 +1,19 @@
 # . should return 0
+# * should return *
+# .. should return 00
+# ** should return **
+# .* should return 1*
+# *. should return *1
 
 
-def minesweeper(mines_map) -> str:
-    return '0'
+def minesweeper(mines_map: str) -> str:
+    if mines_map == '.':
+        return '0'
+    else:
+        return '*'
 
-def test_minesweeper() -> None:
-    data = '.'
-    expected = '0'
+def test_minesweeper_01() -> None:
+    assert minesweeper('.') == '0'
 
-    assert minesweeper(data) == expected
+def test_minesweeper_02() -> None:
+    assert minesweeper('*') == '*'
